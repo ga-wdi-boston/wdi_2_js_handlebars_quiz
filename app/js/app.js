250cb9ba3160c8be274791f3de459266494b92ce
alinaw89 <ali.naw89@gmail.com>
2015-05-08 12:08:54 -0400

    quiz

--- app/js/app.js
+++ app/js/app.js
@@ -12,22 +12,31 @@ var favorites = (function(){
 
   var _renderFoods = function(foods){
     var template = Handlebars.compile($('#food-template').html());
-    $('#foods').html(template({
+    var result = template({foods:foods});
+    $('#food').html(result({
       foods: foods
     }));
   };
 
+
   var _renderMovies = function(movies){
     // your code starts here
-
+    var template = Handlebars.compile($('#movies-template').html());
+    var result = template({movies:movies});
+    $('#song').html(result({
+      movies: movies
+    }));
 
     // your code ends here
   };
 
   var _renderSongs = function(songs){
     // your code starts here
-
-
+    var template = Handlebars.compile($('#songs-template').html());
+    var result = template({songs:songs});
+    $('#movie').html(result({
+      songs: songs
+    }));
     // your code ends here
   };
 
